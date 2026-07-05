8cfe7a1922c4d7892df405306a9cd9e4492f9815
BrianDoce <docena.brian@gmail.com>
2026-07-05 10:30:41 -0700

    Added validation files

--- data-ingestion/artists.py
+++ data-ingestion/artists.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 def get_artist_info(client, artist_id):
-    artist_info = client.get_artist_info(artist_id)
+    raw_artist_info = client.get_artist_info(artist_id)
 
-    artists = {
-        'artist_id': artist_info.get('id'),
-        'artist_name': artist_info.get('name'),
-        'followers': artist_info.get('followers', {}).get('total'),
-        'genres': artist_info.get('genres'),
-        'popularity': artist_info.get('popularity'),
+    artist = {
+        'artist_id': raw_artist_info.get('id'),
+        'artist_name': raw_artist_info.get('name'),
+        'followers': raw_artist_info.get('followers', {}).get('total'),
+        'genres': raw_artist_info.get('genres'),
+        'popularity': raw_artist_info.get('popularity'),
         'ingestion_timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     }
-    return artists, artist_info
+    return artist, raw_artist_info
 
     
 
