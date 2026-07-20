5de45817849bbcc7c5c2dda9e15d6414d12ea9f3
BrianDoce <docena.brian@gmail.com>
2026-07-19 22:06:01 -0700

    fixed dbt tests and added freshness to sources.yml

--- dbt_project/models/staging/stg_albums.sql
+++ dbt_project/models/staging/stg_albums.sql
@@ -3,7 +3,7 @@ SELECT
     album.value:id::STRING AS album_id,
     album.value:name::STRING AS album_name,
     album.value:album_type::STRING AS album_type,
-    album.value:release_date::DATE AS release_date,
+    album.value:release_date::STRING AS release_date,
     album.value:release_date_precision::STRING AS release_date_precision,
     album.value:total_tracks::INTEGER AS total_tracks,
     album.value:external_urls.spotify::STRING AS spotify_url,
@@ -11,5 +11,5 @@ SELECT
     album.value:href::STRING AS api_url,
     ingestion_timestamp
 
-FROM {{ source('bronze', 'bronze_albums') }}
+FROM {{ source('bronze', 'bronze_albums') }},
 LATERAL FLATTEN(input => payload:data) album
--- dbt_project/models/staging/stg_artists.sql
+++ dbt_project/models/staging/stg_artists.sql
@@ -1,7 +1,7 @@
 SELECT
     payload:data:id::VARCHAR AS artist_id,
     payload:data:name::VARCHAR AS artist_name,
-    payload:data:followers::INTEGER AS artist_followers,
+    payload:data:followers.total::INTEGER AS artist_followers,
     payload:data:popularity::INTEGER AS artist_popularity,
     ingestion_timestamp
 FROM {{ source('bronze', 'bronze_artists')}}
