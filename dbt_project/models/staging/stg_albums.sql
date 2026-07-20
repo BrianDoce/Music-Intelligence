bdd00c70a004f31da467a8d90de11eabb8922965
BrianDoce <docena.brian@gmail.com>
2026-07-19 22:11:17 -0700

    Fixed staging

--- dbt_project/models/staging/stg_albums.sql
+++ dbt_project/models/staging/stg_albums.sql
@@ -1,15 +1,38 @@
+WITH albums AS (
+
 SELECT
     payload:artist_id::STRING AS artist_id,
+
     album.value:id::STRING AS album_id,
     album.value:name::STRING AS album_name,
     album.value:album_type::STRING AS album_type,
+
     album.value:release_date::STRING AS release_date,
     album.value:release_date_precision::STRING AS release_date_precision,
+
     album.value:total_tracks::INTEGER AS total_tracks,
+
     album.value:external_urls.spotify::STRING AS spotify_url,
     album.value:uri::STRING AS spotify_uri,
     album.value:href::STRING AS api_url,
-    ingestion_timestamp
 
-FROM {{ source('bronze', 'bronze_albums') }},
-LATERAL FLATTEN(input => payload:data) album
+    ingestion_timestamp,
+
+    ROW_NUMBER() OVER(
+        PARTITION BY album.value:id::STRING
+        ORDER BY ingestion_timestamp DESC
+    ) AS rn
+
+
+FROM {{ source('bronze','bronze_albums') }},
+
+LATERAL FLATTEN(
+    input => payload:data
+) album
+
+
+)
+
+SELECT *
+FROM albums
+WHERE rn = 1
--- dbt_project/models/staging/stg_artists.sql
+++ dbt_project/models/staging/stg_artists.sql
@@ -1,7 +1,21 @@
+WITH ranked AS (
+
 SELECT
-    payload:data:id::VARCHAR AS artist_id,
-    payload:data:name::VARCHAR AS artist_name,
-    payload:data:followers.total::INTEGER AS artist_followers,
-    payload:data:popularity::INTEGER AS artist_popularity,
-    ingestion_timestamp
-FROM {{ source('bronze', 'bronze_artists')}}
+    payload:data:id::STRING AS artist_id,
+    payload:data:name::STRING AS artist_name,
+    payload:data:followers.total::INTEGER AS followers,
+    payload:data:popularity::INTEGER AS popularity,
+    ingestion_timestamp,
+
+    ROW_NUMBER() OVER(
+        PARTITION BY payload:data:id
+        ORDER BY ingestion_timestamp DESC
+    ) AS rn
+
+FROM {{ source('bronze','bronze_artists') }}
+
+)
+
+SELECT *
+FROM ranked
+WHERE rn = 1
